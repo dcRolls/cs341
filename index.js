b10c277b864767da88d2095cc4bb6fc596ca4aab
Jef Rowley <jef.rowley@gmail.com>
2021-05-11 21:46:36 -0500

    ta04

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://jefe:nD3VCmT5FxM8MQX0@cs341.ozi23.mongodb.net/Beemazon?retryWrites=true&w=majority'
+const cors = require('cors');
+
 const User = require('./models/Beemazon/user');
-const DB_URI = 'mongodb+srv://jefe:nD3VCmT5FxM8MQX0@cs341.ozi23.mongodb.net/Beemazon?retryWrites=true&w=majority'
+
 const app = express();
 
 const routes = require('./routes'); //by not listing a specific file in routes, it will use index.js by default
+
+const corsOptions = {
+   origin: "https://prove02-jr.herokuapp.com/",
+   optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+   useUnifiedTopology: true,
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useFindAndModify: false,
+   family: 4
+};
 
 app.use(express.static(path.join(__dirname, 'public')))   
    .set('views', path.join(__dirname, 'views'))
@@ -26,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')))
    .use('/', routes);
 
 mongoose
-   .connect(process.env.MONGODB_URI || DB_URI)
+   .connect(DB_URI, options)
    .then(result => {
       app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
    })
